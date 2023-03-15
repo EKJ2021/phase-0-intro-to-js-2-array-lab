d01611ef0c234b0669e9294a741b54f3251939d8
Emma Watson <emt120412@yahoo.com>
2023-03-15 12:50:53 -0400

    commit tree 4d83942b24041530ae92e3150451352e958c6fbf

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat (name) { 
+    cats.push("Ralph") 
+} 
+function destructivelyPrependCat (name) {
+    cats.unshift ("Bob")
+}
+function destructivelyRemoveLastCat (name) {
+    cats.pop("Garfield")
+}
+function destructivelyRemoveFirstCat (name) {
+    cats.shift("Milo")
+}
+function appendCat (name) {
+    const newCats = [... cats];
+    newCats.push("Broom") 
+    return(newCats);
+}
+function prependCat (name) {
+    const newCats = [... cats];
+    newCats.unshift("Arnold")
+    return(newCats)
+}
+function removeLastCat (name) {
+    const newCats = [... cats];
+    newCats.pop()
+    return(newCats)
+}
+function removeFirstCat (name) {
+    const newCats = [... cats];
+    newCats.shift()
+    return(newCats)
+}
